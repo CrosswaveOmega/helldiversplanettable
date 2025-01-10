834ed58f41b19b40769f5ab8fcf8e7df3ff31dfc
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2025-01-10 15:29:50 -0500

    Update make_historydata.py

--- make_historydata.py
+++ make_historydata.py
@@ -306,10 +306,11 @@ async def get_planet_stats(
                 all_times_new.pop(k)
         all_times_new[dc] = ents
     if timestamp not in all_times_new[dc]:
-        checkv=fetch_entries_by_timestamp(conn,ne.timestamp)
+        checkv=fetch_entries_by_timestamp(conn,str(ne.timestamp))
         if checkv:
             
             print(f"{timestamp} not found in all_times_new[{dc}] but WAS found in db")
+            all_times_new[dc][timestamp] = checkv
             planetstats = all_times_new[dc][timestamp]
             return planetstats
         print(f"{timestamp} not found in all_times_new[{dc}]")
