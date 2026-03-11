8da2391426cdb9f39f38df40f7497a106d0078c4
CrosswaveOmega <xtream2pro@gmail.com>
2026-03-11 14:02:00 -0400

    Update web_utils.py

--- script_making/web_utils.py
+++ script_making/web_utils.py
@@ -32,7 +32,7 @@ async def get_game_stat_at_time(timev: datetime) -> PlanetStatusDict:
     now = datetime.now()
     try:
         url = "https://api-helldivers.kejax.net/api/planets"
-        params = {"time": current_time}
+        params = {"time": int(timev.timestamp())}
         timeout = aiohttp.ClientTimeout(total=60*5)  # Set the timeout to 10 seconds
 
         async with aiohttp.ClientSession(timeout=timeout) as session:
